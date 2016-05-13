d1ae735bfb7f9ae619627ff93492ccdce97f81f5
Karanpal Dewgun <karanpal_dewgun@persistent.co.in>
2016-05-13 15:51:40 +0530

    RouterComponent created

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,44 @@
 import {Component} from '@angular/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
 
 @Component({
   selector: 'my-app',
   template: `
             <h1>{{title}}</h1>
-            <my-heroes></my-heroes>
+            <nav>
+              <a [routerLink]="['Dashboard']">Dashboard</a>
+              <a [routerLink]="['Heroes']">Heroes</a>
+            </nav>
+            <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  directives: [
+    ROUTER_DIRECTIVES
+  ],
+  providers: [
+    HeroService,
+    ROUTER_PROVIDERS
+  ]
 })
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }
+])
 
 export class AppComponent {
   title = 'Tour of Heroes';
